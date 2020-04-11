751a7cd1ff384d761fc445b26bb66a7f415fbb8a
Adrian Aleixandre <aaleixandre@myriadmobile.com>
2020-04-11 08:19:33 -0500

    Added icon + changed description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Adrian Aleixandre - Developer + Designer`,
-    description: `The musings of a front-end developer and designer. Discussion about React, Elm, and other interesting UI/UX topics.`,
+    title: `Adrian Aleixandre`,
+    description: `Developer + Designer: Discussion about React, Elm, and other UI/UX topics.`,
     author: `@_aaleixandre`,
     github: "Vynlar",
     email: "adrian.aleixandre@gmail.com",
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#1A202C`,
         theme_color: `#DD6B20`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/adrian-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
