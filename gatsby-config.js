2ca146770be7a14f20f292759d01e444f65f9cc5
Adrian Aleixandre <aaleixandre@myriadmobile.com>
2020-04-11 08:31:00 -0500

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-163385569-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
